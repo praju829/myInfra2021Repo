496a2f440fe12bd115606db11343ced53358fc8d
praju <102275980+praju829@users.noreply.github.com>
2025-08-14 14:35:52 +0530

    s3-updates

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
-    key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    bucket = "myazterrabucket"
+    key = "Terra-Project"
+    region = "ap-south-1"
+    dynamodb_table = "my-dynamodb-table"
   }
 }
